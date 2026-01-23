55e14224aa386a272e7060f8ad3346553e1c207b
Ryan Plunkett <plunkett.ryanj@gmail.com>
2026-01-22 16:34:56 -0800

    Initial response pull from mai

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import pandas as pd
 
 
 base_url = 'https://statsapi.mlb.com/api/v1/'
+
+response = requests.get(f'{base_url}teams')
+response.raise_for_status()  # Raises HTTPError on 4xx 
+teams_data = response.json()
+teams_df = pd.json_normalize(teams_data, 'teams')
+filtered_df = teams_df[teams_df['name'].str.contains('Angel')]
+print(filtered_df)
