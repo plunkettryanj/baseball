e1f529ad7798e4bb0c4dabcb3a4fd4f9ebdab385
Ryan Plunkett <plunkett.ryanj@gmail.com>
2026-02-02 16:26:27 -0800

    Adding metadata information for endpoints and initiate testing some for validation

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import requests
 import pandas as pd
 
 
+live_game_feed = f'https://statsapi.mlb.com/api/v1.1/game/{game_list_input[i]}/feed/live'
+sample_reg_season = f'https://statsapi.mlb.com/api/v1/schedule/?sportId={sport_id_str}&gameTypes={game_type_str}&season={year_input_str}&hydrate=lineup,players'
+player_game_log = f'http://statsapi.mlb.com/api/v1/people/{player_id}?hydrate=stats(group={group},type=gameLog,season={season},startDate={start_date},endDate={end_date},sportId={sport_id},gameType=[{game_type_str}]),hydrations'
 base_url = 'https://statsapi.mlb.com/api/v1/'
-
-response = requests.get(f'{base_url}teams')
-response.raise_for_status()  # Raises HTTPError on 4xx 
-teams_data = response.json()
-teams_df = pd.json_normalize(teams_data, 'teams')
-filtered_df = teams_df[teams_df['name'].str.contains('Angel')]
-print(filtered_df)
+endpoints = ['teams', 'sports', 'gameTypes', 'leagues']
+for endpoint in endpoints:
+    response = requests.get(f'{base_url}{endpoint}')
+    response.raise_for_status()  # Raises HTTPError on 4xx 
+    data = response.json()
+    normalize_df = pd.json_normalize(data, endpoint)
+    print(normalize_df.head())
